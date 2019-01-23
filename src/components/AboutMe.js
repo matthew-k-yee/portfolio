ffa413b607ead894175c9003698257133934212a
Matthew Yee <matthewyee@ool-ad020572.dyn.optonline.net>
2019-01-23 11:18:24 -0500

    link google drive

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,6 +8,7 @@ function AboutMe(props){
       <h1>About Me</h1>
       <p>"Simplicity is about subtracting the obvious and adding the meaningful."<cite> -John Maeda</cite></p>
       <p>I am a full stack developer. Excited to use and further develop my technical skills in collaborative and cohesive environments. Adept at creating responsive Full-Stack CRUD websites from concept to design to deployment. I enjoy working with dynamic team members and contributing to innovative projects. </p>
+      <a className='resume' href='https://drive.google.com/file/d/1hlU_fpJe6t6mB3qpP5NFk0MKBzF6Rz5b/view'>View My Resume</a>
     </div>
   )
 }
