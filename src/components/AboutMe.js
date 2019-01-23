707f5ee61de9c4ed541aee357940576b141074f2
Matthew Yee <matthewyee@ool-ad020572.dyn.optonline.net>
2019-01-23 11:07:12 -0500

    remove pdf code

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,6 @@ function AboutMe(props){
       <h1>About Me</h1>
       <p>"Simplicity is about subtracting the obvious and adding the meaningful."<cite> -John Maeda</cite></p>
       <p>I am a full stack developer. Excited to use and further develop my technical skills in collaborative and cohesive environments. Adept at creating responsive Full-Stack CRUD websites from concept to design to deployment. I enjoy working with dynamic team members and contributing to innovative projects. </p>
-      {/* <a className='resume' href={Pdf}>View My Resume</a> */}
     </div>
   )
 }
