4ce95266a7e43472f06c928067506439c995c3b5
Matthew Yee <matthewyee@ool-ad020572.dyn.optonline.net>
2019-01-22 19:25:43 -0500

    react helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet'
+
 import Nav from './components/Nav'
 import Name from './components/Name'
 import AboutMe from './components/AboutMe'
@@ -14,6 +16,9 @@ class App extends Component {
         <div className="App">
           <div className='background'>
             <Nav />
+            <Helmet>
+               <title>Matthew K Yee</title>
+            </Helmet>
             <Route exact path ="/" component={Name}/>
             <Route path ="/aboutme" component={AboutMe}/>
             <Route path ="/projects" component={Projects}/>
